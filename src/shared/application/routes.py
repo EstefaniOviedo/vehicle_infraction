1a4e6fc3e91dbb18a1f7064de0577f71d4dc30cc
EstefaniOviedo <estefani@karmapulse.com>
2024-07-19 17:50:05 -0600

    feat(routes): Add person_router

--- src/shared/application/routes.py
+++ src/shared/application/routes.py
@@ -4,6 +4,7 @@ main_router = APIRouter()
 
 from src.user.application.user import router as user_router
 from src.user.application.official import router as official_router
+from src.user.application.person import router as person_router
 
 
 # Root
@@ -11,5 +12,7 @@ from src.user.application.official import router as official_router
 def root():
     return {"message": "Hello World!"}
 
+
 main_router.include_router(user_router)
 main_router.include_router(official_router)
+main_router.include_router(person_router)

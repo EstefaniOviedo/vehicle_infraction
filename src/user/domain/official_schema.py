df4ecdaf0615dc1f73597e788a950441cddb5904
EstefaniOviedo <estefani@karmapulse.com>
2024-07-19 17:51:00 -0600

    refactor(official_schema): Delete properties in Official and add ResponseOfficialCreate

--- src/user/domain/official_schema.py
+++ src/user/domain/official_schema.py
@@ -1,11 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
 
 class Official(BaseModel):
+    email: str
     name: str
-    official_key: str
     password: str
-    
+
+
 class ResponseOfficial(BaseModel):
-    _id: str
+    id: str = Field(..., alias="_id")
     name: str
     official_key: str
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class ResponseOfficialCreate(BaseModel):
+    message: str
+    data: ResponseOfficial

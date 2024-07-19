805c13c0a42dad24d32afb00d09194a23ca8d22d
EstefaniOviedo <estefani@karmapulse.com>
2024-07-19 16:41:31 -0600

    refactor(official): Change in get_officials, insert_official, update_official_inf and delete_official_inf

--- src/user/infrastructure/official.py
+++ src/user/infrastructure/official.py
@@ -1,8 +1,12 @@
 import uuid
 from fastapi import HTTPException
 from datetime import datetime
-from bson import ObjectId
-from src.shared.infrastructure.db.crud import get_items, inset_item, update, get_item
+from src.shared.infrastructure.db.crud import (
+    get_items,
+    inset_item,
+    update,
+    get_item_id,
+)
 from src.shared.infrastructure.config import get_settings
 from src.user.infrastructure.user import get_password_hash
 
@@ -10,60 +14,83 @@ SETTINGS = get_settings()
 
 collection = "users"
 
+
 async def generate_employee_id():
-    #Pref for ids
+    # Pref for ids
     prefix = "EMP"
-    
+
     # date format YYYYMMDDHHMMSS
     now = datetime.now().strftime("%Y%m%d%H%M%S")
-    
+
     # UUID4 for generate idenntifier random unique
     unique_suffix = uuid.uuid4().hex[:6]
-    
 
     employee_id = f"{prefix}{now}{unique_suffix}"
-    
+
     return employee_id
 
+
 async def get_officials():
-    result = get_items(collection)
+    filter = {"type": "oficial", "active": True}
+    projection = {"_id": 1, "name": 1, "official_key": 1}
+    result = await get_items(collection, filter, projection)
+
+    for item in result:
+        item["_id"] = str(item.get("_id"))
     return result
 
+
 async def insert_official(data):
-    try: 
+    try:
         new_official_id = await generate_employee_id()
-        password = get_password_hash(data.get("password"))
+        password = await get_password_hash(data.get("password"))
         official = {
-            "fullname": data.get("name"),
+            "name": data.get("name"),
+            "email": data.get("email"),
             "official_key": new_official_id,
             "password": password,
+            "active": True,
+            "type": "oficial",
+            "scopes": ["oficial"],
             "created_at": datetime.now(),
             "updated_at": datetime.now(),
-            "type": "oficial",
         }
-        result = inset_item(collection, official)
+        result_id = await inset_item(collection, official)
+        official_respon = {
+            "name": data.get("name"),
+            "official_key": new_official_id,
+        }
+        result = {
+            "_id": result_id,
+            **official_respon,
+        }
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-async def update_official_inf(_id, data):
-    find_official = get_item(collection, { "_id", ObjectId(_id)})
-    if find_official is None:
-        raise HTTPException(status_code=404, detail="Oficial no encontrado")
-    official_dic = dict(data)
-    response = {
-        "_id": _id,
-        **official_dic
-    }
-    update(collection, official_dic,_id)
-    return response
 
-async def delete_official_inf(_id, data):
-    find_official = get_item(collection, { "_id", ObjectId(_id)})
+async def update_official_inf(_id, data):
+    official_dic = dict(data)
+    find_off = await get_item_id(collection, _id)
+    if find_off is None:
+        raise HTTPException(status_code=404, detail="Oficial no encontrado")
+    official_dic["updated_at"] = datetime.now()
+    await update(collection, official_dic, _id)
+    official_respon = {
+        "name": official_dic.get("name"),
+        "email": official_dic.get("email"),
+    }
+    result = {
+        "_id": _id,
+        **official_respon,
+    }
+    return result
+
+
+async def delete_official_inf(_id):
+    find_official = await get_item_id(collection, _id)
     if find_official is None:
         raise HTTPException(status_code=404, detail="Oficial no encontrado")
-    official_dic = {
-        "active": False
-    }
-    update(collection, official_dic,_id)
-    
+    official_dic = {"active": False}
+    result = await update(collection, official_dic, _id)
+    return result

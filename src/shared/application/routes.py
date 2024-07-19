1c15ac546b1b8a01e3e3901da3195edb02136032
EstefaniOviedo <estefani@karmapulse.com>
2024-07-19 16:42:50 -0600

    feat(routes): Add user_router

--- src/shared/application/routes.py
+++ src/shared/application/routes.py
@@ -2,11 +2,14 @@ from fastapi import APIRouter
 
 main_router = APIRouter()
 
-from src.user.application.official import router as official_router  # noqa: E402
+from src.user.application.user import router as user_router
+from src.user.application.official import router as official_router
+
 
 # Root
 @main_router.get("/")
 def root():
     return {"message": "Hello World!"}
 
+main_router.include_router(user_router)
 main_router.include_router(official_router)

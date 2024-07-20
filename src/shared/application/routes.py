8d83e266ec46fd04606c69d3ee09ed167b45a626
EstefaniOviedo <estefani@karmapulse.com>
2024-07-20 01:03:04 -0600

    feat(routes): Add infraction_router

--- src/shared/application/routes.py
+++ src/shared/application/routes.py
@@ -6,6 +6,7 @@ from src.user.application.user import router as user_router
 from src.user.application.official import router as official_router
 from src.user.application.person import router as person_router
 from src.vehicle.application.vehicle import router as vehicle_router
+from src.vehicle.application.infraction import router as infraction_router
 
 
 # Root
@@ -18,3 +19,4 @@ main_router.include_router(user_router)
 main_router.include_router(official_router)
 main_router.include_router(person_router)
 main_router.include_router(vehicle_router)
+main_router.include_router(infraction_router)
